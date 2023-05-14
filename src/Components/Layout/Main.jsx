d99f840ca4b237b6ec6404d5640278579316b23b
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-05-14 08:28:42 +0600

    navbar added

--- src/Components/Layout/Main.jsx
+++ src/Components/Layout/Main.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import { Toaster } from "react-hot-toast";
+
 const Main = () => {
   return (
     <div>
-      <h1>Main</h1>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Toaster />
     </div>
   );
 };
